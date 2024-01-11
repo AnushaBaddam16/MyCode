51956efb40b2b85d7fcf7a2a397e267146057923
anushabaddam16 <anureddyb.work@gmail.com>
2024-01-11 11:24:11 -0600

    Student Class Created

---
+++ Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    String name ="Anusha";
+    public String getName(){
+        return name;
+    }
+    public static void main(String args[]){
+        Student s1=new Student();
+        System.out.println(s1.getName());
+    }
+}
